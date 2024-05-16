19a159cf93cf219809da22423e09f4393624cdc5
seemerslx <alexbiletskyi17@gmail.com>
2024-05-17 00:30:54 +0300

    Added Discount.Grpc microservice.

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Data
 {
     public class DiscountContext(DbContextOptions<DiscountContext> opts) : DbContext(opts)
     {
-        public DbSet<CouponModel> Coupons => Set<CouponModel>();
+        public DbSet<Coupon> Coupons => Set<Coupon>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>()
+                .HasData(new Coupon[]
+                {
+                    new Coupon() { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                    new Coupon() { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 },
+                });
+        }
     }
 }
